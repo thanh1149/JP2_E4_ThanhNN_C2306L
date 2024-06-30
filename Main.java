cfc2ecdd5726f3cd17c5b7ecbc19c1d5c9c5eef8
thanh1149 <155382289+thanh1149@users.noreply.github.com>
2024-07-01 03:30:07 +0700

    JP2_E4_ThanhNN_C2306L

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+    }
+}
